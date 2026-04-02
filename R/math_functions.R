c179da6570f230ce303e4cdd4ae47ce65d93c57e
Alicele07 <alicele280303@gmail.com>
2026-04-02 13:05:32 -0700

    made the first version of our basic package

---
+++ R/math_functions.R
@@ -0,0 +1,25 @@
+#' Add two numbers
+#' @param a A number
+#' @param b A number
+#'
+#' @returns the sum of a and b
+#' @export
+#'
+#' @examples
+#' add_numbers(2, 2)
+add_numbers <- function(a, b) {
+  return (a + b)
+}
+
+#' Multiply two numbers
+#' @param a A number
+#' @param b A number
+#'
+#' @returns the product of a and b
+#' @export
+#'
+#' @examples
+#' multiply_numbers(2, 2)
+multiply_numbers <- function(a, b) {
+  return (a * b)
+}
